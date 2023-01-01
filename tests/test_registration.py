044bad89674c216e4547bf93ce8de73b35976011
Petr Popov <popovpn7@gmail.com>
2023-01-01 19:17:05 +0300

    Add test: test_registration_user_with_empty_field

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -21,3 +21,25 @@ class TestRegistration:
 
         registration_success_page.should_be_open_registration_success_page()
         registration_success_page.should_be_confirm_register_message()
+
+    @title('Регистрация пользователя с пустыми полями в форме регистрации')
+    def test_registration_user_with_empty_field(self, main_page, header, registration_page,
+                                                data_registration_emptyfields):
+        main_page.open()
+        main_page.should_be_open_main_page()
+
+        header.go_to_register_page()
+        registration_page.should_be_open_registration_page()
+
+        registration_page.fill_registration_form(
+            is_random=False,
+            gender=data_registration_emptyfields[0],
+            firstname=data_registration_emptyfields[1],
+            lastname=data_registration_emptyfields[2],
+            email=data_registration_emptyfields[3],
+            password=data_registration_emptyfields[4],
+            confirm_password=data_registration_emptyfields[5],
+        )
+        registration_page.click_on_register_button()
+
+        registration_page.can_see_error_message_with_text(expected_error_message=data_registration_emptyfields[6])
