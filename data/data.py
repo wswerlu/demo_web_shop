328476767ae838bafa32f1e0b0a408d15ad9ed39
Petr Popov <popovpn7@gmail.com>
2023-01-07 17:59:57 +0300

    Add constant PRODUCTS_WHO_CAN_ADD_TO_WISHLIST

--- data/data.py
+++ data/data.py
@@ -26,3 +26,27 @@ CREDIT_CARD_TYPE = {
     'Amex': CardType.AMERICAN_EXPRESS,
     'Discover': None,
 }
+
+PRODUCTS_WHO_CAN_ADD_TO_WISHLIST = [
+    'health',
+    'copy-of-tcp-self-paced-training-2',
+    'copy-of-copy-of-copy-of-copy-of-tcp-self-paced-training',
+    'copy-of-tcp-self-paced-training',
+    'copy-of-copy-of-copy-of-tcp-self-paced-training',
+    'tcp',
+    'copy-of-tcp-self-paced-training-3',
+    'phone-cover',
+    'smartphone',
+    'mens-wrinkle-free-long-sleeve',
+    'blue-and-green-sneaker',
+    '50s-rockabilly-polka-dot-top-jr-plus-size',
+    'album-3',
+    'music-album-1',
+    'music-2',
+    'create-it-yourself-jewelry',
+    'black-white-diamond-heart',
+    '5-virtual-gift-card',
+    '25-virtual-gift-card',
+    '50-physical-gift-card',
+    '100-physical-gift-card',
+]
