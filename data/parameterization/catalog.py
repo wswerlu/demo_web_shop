bba76fcf2312972d9c2891c77d03e2afe15ead80
Petr Popov <91988704+wswerlu@users.noreply.github.com>
2023-01-08 00:39:46 +0300

    Test sort products by price (#19)

--- data/parameterization/catalog.py
+++ data/parameterization/catalog.py
@@ -2,3 +2,8 @@ alphabet = [
     ['Name: A to Z', False, 'Sort by: Name: A to Z'],
     ['Name: Z to A', True, 'Sort by: Name: Z to A'],
 ]  # данные для теста test_sort_products_by_alphabet
+
+price = [
+    ['Price: Low to High', False, 'Sort by: Price: Low to High'],
+    ['Price: High to Low', True, 'Sort by: Price: High to Low'],
+]  # данные для теста test_sort_products_by_price
--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -18,3 +18,14 @@ class TestCatalog:
 
         catalog_page.sort_by(method=data_catalog_alphabet[0])
         catalog_page.should_be_sort_by_alphabet(is_reverse=data_catalog_alphabet[1])
+
+    @title('Сортировка товаров по цене')
+    def test_sort_products_by_price(self, main_page, header, catalog_page, data_catalog_price):
+        main_page.open()
+        main_page.should_be_open_main_page()
+
+        category_name = header.click_on_random_category_and_get_its_name()
+        catalog_page.should_be_open_catalog_page_by_title(category_name=category_name)
+
+        catalog_page.sort_by(method=data_catalog_price[0])
+        catalog_page.should_be_sort_by_price(is_reverse=data_catalog_price[1])
