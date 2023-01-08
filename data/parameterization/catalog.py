205970a7e6c75a5b46d023f93e1dea7eae8a817a
Petr Popov <popovpn7@gmail.com>
2023-01-08 15:01:22 +0300

    Add data for paramatrization of test_number_of_products_per_page

--- data/parameterization/catalog.py
+++ data/parameterization/catalog.py
@@ -7,3 +7,9 @@ price = [
     ['Price: Low to High', False, 'Sort by: Price: Low to High'],
     ['Price: High to Low', True, 'Sort by: Price: High to Low'],
 ]  # данные для теста test_sort_products_by_price
+
+display = [
+    [4, 'Display 4 products per page'],
+    [8, 'Display 8 products per page'],
+    [12, 'Display 12 products per page'],
+]  # данные для теста test_number_of_products_per_page
