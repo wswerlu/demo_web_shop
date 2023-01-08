7fd5c16c69ec19b74f432c17ca8b33a33a7ea1ff
Petr Popov <popovpn7@gmail.com>
2023-01-08 15:04:16 +0300

    Add test: test_number_of_products_per_page

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -29,3 +29,14 @@ class TestCatalog:
 
         catalog_page.sort_by(method=data_catalog_price[0])
         catalog_page.should_be_sort_by_price(is_reverse=data_catalog_price[1])
+
+    @title('Количество товаров на странице')
+    def test_number_of_products_per_page(self, main_page, header, catalog_page, data_catalog_display):
+        main_page.open()
+        main_page.should_be_open_main_page()
+
+        category_name = header.click_on_random_category_and_get_its_name()
+        catalog_page.should_be_open_catalog_page_by_title(category_name=category_name)
+
+        catalog_page.choose_page_size(page_size=data_catalog_display[0])
+        catalog_page.should_be_number_of_products_displayed_per_page(page_size=data_catalog_display[0])
