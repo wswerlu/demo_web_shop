fd54345d6a3ec3f7ddf07dc4c5d3d1efc360deb7
Petr Popov <popovpn7@gmail.com>
2023-01-14 01:45:35 +0300

    Change click_on_random_category_and_get_its_name method to click_on_category_with_name

--- locators/header_locators.py
+++ locators/header_locators.py
@@ -10,9 +10,6 @@ class HeaderLocators:
     LOGOUT_LINK = (By.CSS_SELECTOR, 'a.ico-logout')
     LOGIN_LINK = (By.CSS_SELECTOR, 'a.ico-login')
     ACCOUNT_LINK = (By.XPATH, ".//a[@class='account' and text()='{0}']")
-    CATEGORIES = (By.CSS_SELECTOR, 'ul.top-menu > li')
-    SUBLIST_FIRST_LEVEL = (By.CSS_SELECTOR, 'ul.sublist.firstLevel')
-    SUBLIST_CATEGORIES = (By.CSS_SELECTOR, 'ul.sublist.firstLevel a')
     PRODUCT_QUANTITY_IN_CART = (By.CSS_SELECTOR, 'a.ico-cart span.cart-qty')
     CART_LINK = (By.CSS_SELECTOR, 'a.ico-cart')
     PRODUCTS_IN_FLYOUT_CART = (By.CSS_SELECTOR, 'div.mini-shopping-cart div.item')
@@ -21,3 +18,8 @@ class HeaderLocators:
     PRODUCT_QUANTITY_IN_FLYOUT_CART = (By.CSS_SELECTOR, 'div.quantity span')
     PRODUCT_QUANTITY_IN_WISHLIST = (By.CSS_SELECTOR, 'a.ico-wishlist span.wishlist-qty')
     WISHLIST_LINK = (By.CSS_SELECTOR, 'a.ico-wishlist')
+    MAIN_CATEGORY_BY_SUBCATEGORY_NAME = (
+        By.XPATH,
+        ".//ul[@class='top-menu']//a[contains(text(), '{0}')]/../../preceding-sibling::a",
+    )
+    CATEGORY_BY_NAME = (By.XPATH, ".//ul[@class='top-menu']//a[contains(text(), '{0}')]")
